5818720fbbacacc69fc5b7bd902b4c43c5bb29ab
prozaqq <shaishnaider@gmail.com>
2019-01-24 18:05:52 +0200

    deleted comments

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from brute import Worker
 
 def main():
 
-    proxies.load_proxylist() # puts the proxies in a queue inside proxy_list variable
-    accounts.load_accounts() # puts the accounts in queue file
+    proxies.load_proxylist() 
+    accounts.load_accounts() 
 
     print('* Number of proxies loaded : ' + str(proxies.proxy_list.qsize()))
     print('* Number of accounts loaded : ' + str(accounts.accounts_list.qsize()))
